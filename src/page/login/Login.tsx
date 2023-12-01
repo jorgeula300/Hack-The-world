ac10c37560787f60907bffc76035157015c6c524
Samuel Palacios <palaciosgarridosamuel@gmail.com>
2023-12-01 15:59:23 -0500

    Login v1

--- src/page/login/Login.tsx
+++ src/page/login/Login.tsx
@@ -1,5 +1,12 @@
 const Login = () => {
-    return (<h1>hola desde login </h1> );
+    return (<div className="login">
+        <h1>Login</h1>
+        <form action="" className="formulario" method="post">
+            <input type="text" placeholder="correo Electronico" name="correo" id="correo" required></input>
+            <input type="password" placeholder="contraseña" name="contrasena" id="contrasena" required></input>
+            <button type="submit">Entrar</button>
+        </form>
+    </div>);
 }
 
 export default Login;
